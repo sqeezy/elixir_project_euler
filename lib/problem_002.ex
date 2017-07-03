fdb3045c33ebe6f4009c542a9757f5a80c297adb
Anton Herzog <info@antonherzog.com>
2017-07-03 22:53:25 +0200

    Refactoring

--- lib/problem_002.ex
+++ lib/problem_002.ex
@@ -2,14 +2,10 @@ defmodule Problem002 do
   require Integer
   @moduledoc false
   
+  def fibonacci(n) when (n < 0),do: raise(ArgumentError, "Fibonacci sequence is not defined for negative input.")
   def fibonacci(0), do: 0
   def fibonacci(1), do: 1
-  def fibonacci(n) when (n < 0) do
-    raise(ArgumentError, "Fibonacci sequence is not defined for negative input.")
-  end
-  def fibonacci(n) do
-    fibonacci(n - 1) + fibonacci(n - 2)
-  end
+  def fibonacci(n), do: fibonacci(n - 1) + fibonacci(n - 2)
 
   def sum_of_even_fibonacci_numbers_under(border) do
     numbers_under_border = fibonacci_numbers_under(border)
