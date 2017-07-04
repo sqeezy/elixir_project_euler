50679778b6cecd52b2110b6d73c273e53e17ed2c
Anton Herzog <info@antonherzog.com>
2017-07-04 22:35:27 +0200

    Add test for problem 5

---
+++ test/problem_005_test.exs
@@ -0,0 +1,12 @@
+defmodule Problem005Test do
+  use ExUnit.Case
+
+  @tag timeout: 120000
+  test "find smallest number evenly divisible from 1 to 20" do
+    divisors = 1..20
+    num = 1..1000000000
+            |> Stream.filter(fn n -> Enum.all?(divisors,&(rem(n, &1)==0)) end)
+            |> Enum.take(1)
+    assert [num] == [232792560]
+  end
+end
