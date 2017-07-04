a42d4c70f38779554d77bb3eaaae62c18f03a470
Anton Herzog <info@antonherzog.com>
2017-07-04 22:42:31 +0200

    Skip long running test

--- test/problem_005_test.exs
+++ test/problem_005_test.exs
@@ -2,6 +2,7 @@ defmodule Problem005Test do
   use ExUnit.Case
 
   @tag timeout: 120000
+  @tag :long_running
   test "find smallest number evenly divisible from 1 to 20" do
     divisors = 1..20
     num = 1..1000000000
